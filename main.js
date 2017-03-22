d57889fadd7f7a4fa46dca8be7c664f65df85bb8
topoftheyear <topoftheyear@Gmail.com>
2017-03-21 20:03:16 -0500

    Added CORS attempts

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 var stage;
 var gameWorld;
+var size = 1000;
+
+var queue;
 
 function load(){
-    init();
+    queue = new createjs.LoadQueue(false);
+    queue.on("complete", init, once=true);
+    var loadItem = new createjs.LoadItem();
+    loadItem.set({id:"testImage",src:"Images/Test.png"});
+    queue.loadFile(loadItem);
 }
 
 function init(){
     gameWorld = new createjs.Container();
     stage = new createjs.Stage("canvas");
     
+    generateWorld();
+    
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", tick);
     
     stage.addChild(gameWorld);
     
-    this.document.onkeydown = keyDown;
+    //this.document.onkeydown = keyDown;
 }
 
 function keyDown(event){
@@ -24,4 +33,41 @@ function keyDown(event){
 
 function tick(event){
     stage.update(event);
+}
+
+function generateWorld(){
+    map = new Array(size);
+    for (var i = 0; i < map.length; i++){
+        map[i] = new Array(size);
+    }
+    
+    var imageData = queue.getResult("testImage");
+    ssData.images.push(imageData);
+    
+    var testSheet = new createjs.SpriteSheet(generateSpriteSheet(ssData, 32, 32, 10, {exist:[0]}));
+    
+    for (var i = 0; i < map.length; i++){
+        for (var j = 0; j < map.length; j++){
+            var block;
+            
+            block = new createjs.Sprite(testSheet, "exist");
+            
+            block.x = i * 32;
+            block.y = j * 32;
+            gameWorld.addChild(block);
+        }
+    }
+}
+
+function generateSpriteSheet(source, w, h, fps, animation){
+    var img = new Image();
+    img.crossOrigin="Anonymous";
+    img.src = source;
+    var data = {
+        images: [img],
+        frames: {width:w, height:h},
+        framerate: fps,
+        animations: animation
+    }
+    return data;
 }

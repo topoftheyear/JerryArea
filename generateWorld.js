e9de070bea8f5cc0493bfb5da21cc327e42c814f
topoftheyear <topoftheyear@Gmail.com>
2017-04-05 15:55:32 -0500

    Added hills and grass generation with 33% accuracy

--- generateWorld.js
+++ generateWorld.js
@@ -1,5 +1,12 @@
 function generateWorld(){
-    map = new Array(size.width);
+    function addBlock(i, j, block){
+		if (map[i][j].numChildren > 0){
+			map[i][j].removeAllChildren();
+		}
+		map[i][j].addChild(block);
+	}
+	
+	map = new Array(size.width);
     for (var i = 0; i < map.length; i++){
         map[i] = new Array(size.height);
     }
@@ -24,15 +31,47 @@ function generateWorld(){
 			map[i][j] = tempContainer;
         }
     }
+	alert("containers added");
 	
 	// Generate a basic ground and sky
 	for (var i = 0; i < size.width; i++){
 		for (var j = 0; j < size.height; j++){
 			if (j > 100){
-                map[i][j].addChild(block = new createjs.Sprite(stoneSheet));
+                addBlock(i, j, block = new createjs.Sprite(stoneSheet));
             } else if (j > 50){
-				map[i][j].addChild(block = new createjs.Sprite(dirtSheet));
+				addBlock(i, j, block = new createjs.Sprite(dirtSheet));
             }
 		}
 	}
+	alert("ground and sky added");
+	
+	// Generate hills
+	var startingHeight = randomNumber(20, 100);
+	for (var i = 0; i < size.width; i++){
+		for (var j = startingHeight; j <= 100; j++){
+			addBlock(i, j, block = new createjs.Sprite(dirtSheet));
+		}
+		if (randomNumber(1,3) === randomNumber(1,3)){
+			startingHeight = randomNumber(startingHeight - 2, startingHeight + 2);
+		} else{
+			startingHeight = randomNumber(startingHeight - 1, startingHeight + 1);
+		}
+		
+		if (startingHeight > 100){
+			startingHeight = 100;
+		} else if (startingHeight < 20){
+			startingHeight = 20;
+		}
+	}
+	alert("hills made");
+	
+	// Make all dirt blocks that have air above them be grass
+	for (var i = 0; i < size.width; i++){
+		for (var j = 0; j < size.height - 1; j++){
+			if (map[i][j].numChildren === 0 && map[i][j + 1].numChildren != 0 && map[i][j + 1].getChildAt(0).spriteSheet === dirtSheet){
+				addBlock(i, j + 1, block = new createjs.Sprite(grassSheet));
+			}
+		}
+	}
+	alert("grassified");
 }
